42e97b977aa818106d15b4c838db1c10bd7a48c6
Dalton Okechukwu <dalton.0x0@gmail.com>
2023-07-01 09:44:41 -0600

    adds welcome header to home

--- client/src/routes/home/home.component.tsx
+++ client/src/routes/home/home.component.tsx
@@ -1,13 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
-import Directory from '../../components/directory/directory.component';
+import Directory from "../../components/directory/directory.component";
+import { HomeContainer } from "./home.styles";
 
 const Home = () => {
   return (
-    <div>
-      <Directory />
-      <Outlet />
-    </div>
+    <HomeContainer>
+      <div>
+        <span>Welcome, please sign in or sign up to continue</span>
+        <Directory />
+        <Outlet />
+      </div>
+    </HomeContainer>
   );
 };
 
