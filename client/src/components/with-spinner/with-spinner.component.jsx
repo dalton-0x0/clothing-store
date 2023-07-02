ef8ba66a814054e1ff3861c5257e5e6ef67b13a2
Dalton Okechukwu <dalton.0x0@gmail.com>
2023-07-02 02:42:28 -0600

    removes contact nav link. updates withSpinner fn call

--- client/src/components/with-spinner/with-spinner.component.jsx
+++ client/src/components/with-spinner/with-spinner.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { SpinnerContainer, SpinnerOverlay } from "./with-spinner.styles";
 
-const WithSpinner = WrappedComponent => {
+const WithSpinner = (WrappedComponent) => {
   const Spinner = ({ isLoading, ...otherProps }) => {
     return isLoading ? (
       <SpinnerOverlay>
@@ -14,5 +14,7 @@ const WithSpinner = WrappedComponent => {
   };
   return Spinner;
 };
+
+// update firestore (not realtime database!) rules if allow read, write is false
 
 export default WithSpinner;
